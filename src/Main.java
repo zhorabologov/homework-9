707412e184f8ed39c48b617bc1b44303cd67bdf4
Жора <zhorabologov@gmal.com>
2023-02-27 14:49:54 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1() {
 
+        String fistName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + fistName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    public static void task2() {
+
+        String fullName = "Ivanov Ivan Ivanovich";
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  " + upperFullName);
+    }
+
+    public static void task3() {
+        String fullName = "Семёнов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName.replace("ё", "е"));
     }
 
 }
